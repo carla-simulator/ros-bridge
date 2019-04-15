97ca3a021eceda9c4d219192a48aae99ef80b9d1
Pasch, Frederik <frederik.pasch@intel.com>
2019-04-15 18:11:08 +0200

    Add initial Binding base class

---
+++ carla_ros_bridge/src/carla_ros_bridge/binding/binding.py
@@ -0,0 +1,13 @@
+#
+# Copyright (c) 2019 Intel Corporation
+#
+# This work is licensed under the terms of the MIT license.
+# For a copy, see <https://opensource.org/licenses/MIT>.
+#
+class VehicleClass(object):
+    UNKNOWN = 0
+    BIKE = 5
+    CAR = 6
+    TRUCK = 7
+    MOTORCYCLE = 8
+    VEHICLE = 10
