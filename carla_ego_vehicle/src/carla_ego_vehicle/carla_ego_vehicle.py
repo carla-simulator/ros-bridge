3ebe949dbd8405dfa51bfe56f94e4c66c19458e9
Pasch, Frederik <frederik.pasch@intel.com>
2019-07-04 12:51:21 +0200

    carla_ego_vehicle: Make sensor_tick optional in lidar

--- carla_ego_vehicle/src/carla_ego_vehicle/carla_ego_vehicle.py
+++ carla_ego_vehicle/src/carla_ego_vehicle/carla_ego_vehicle.py
@@ -186,7 +186,10 @@ class CarlaEgoVehicle(object):
                     bp.set_attribute('upper_fov', str(sensor_spec['upper_fov']))
                     bp.set_attribute('lower_fov', str(sensor_spec['lower_fov']))
                     bp.set_attribute('points_per_second', str(sensor_spec['points_per_second']))
-                    bp.set_attribute('sensor_tick', str(sensor_spec['sensor_tick']))
+                    try:
+                        bp.set_attribute('sensor_tick', str(sensor_spec['sensor_tick']))
+                    except KeyError:
+                        pass
                     sensor_location = carla.Location(x=sensor_spec['x'], y=sensor_spec['y'],
                                                      z=sensor_spec['z'])
                     sensor_rotation = carla.Rotation(pitch=sensor_spec['pitch'],
