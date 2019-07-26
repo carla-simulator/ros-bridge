646b40d04c89f79ea0c2ee4ec1ad5e82723c6d5c
Pasch, Frederik <frederik.pasch@intel.com>
2019-07-26 15:08:04 +0200

    Support waiting for vehicle control command within synchronous mode

--- carla_ros_bridge/src/carla_ros_bridge/carla_status_publisher.py
+++ carla_ros_bridge/src/carla_ros_bridge/carla_status_publisher.py
@@ -21,13 +21,14 @@ class CarlaStatusPublisher(object):
     report the carla status
     """
 
-    def __init__(self, synchronous_mode):
+    def __init__(self, synchronous_mode, fixed_delta_seconds):
         """
         Constructor
 
         """
         self.synchronous_mode = synchronous_mode
         self.synchronous_mode_running = True
+        self.fixed_delta_seconds = fixed_delta_seconds
         self.frame = 0
         self.carla_status_publisher = rospy.Publisher(
             "/carla/status", CarlaStatus, queue_size=10, latch=True)
@@ -42,6 +43,7 @@ class CarlaStatusPublisher(object):
         status_msg.frame = self.frame
         status_msg.synchronous_mode = self.synchronous_mode
         status_msg.synchronous_mode_running = self.synchronous_mode_running
+        status_msg.fixed_delta_seconds = self.fixed_delta_seconds
         self.carla_status_publisher.publish(status_msg)
 
     def set_synchronous_mode_running(self, running):
