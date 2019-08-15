294258069310575a249835a37f8cae20a3019b68
Pasch, Frederik <frederik.pasch@intel.com>
2019-08-15 15:40:30 +0200

    Support different ego vehicle rolenames in pclrecorder

--- pcl_recorder/src/PclRecorder.cpp
+++ pcl_recorder/src/PclRecorder.cpp
@@ -19,7 +19,11 @@ PclRecorder::PclRecorder()
   }
 
   // Create a ROS subscriber for the input point cloud
-  sub = nh.subscribe("/carla/ego_vehicle/lidar/lidar1/point_cloud", 1000000, &PclRecorder::callback, this);
+  std::string roleName;
+  if (!ros::param::get("~role_name", roleName)) {
+    roleName = "ego_vehicle";
+  }
+  sub = nh.subscribe("/carla/" + roleName + "/lidar/lidar1/point_cloud", 1000000, &PclRecorder::callback, this);
 }
 
 void PclRecorder::callback(const pcl::PCLPointCloud2::ConstPtr& cloud)
