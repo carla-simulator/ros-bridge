e7907330850e21483cd4571ef6657a02d3b8148c
Bruno Brito <bruno.debrito@tudelft.nl>
2020-02-25 17:38:49 +0100

    stopping before reseting

--- carla_ego_vehicle/src/carla_ego_vehicle/carla_ego_vehicle.py
+++ carla_ego_vehicle/src/carla_ego_vehicle/carla_ego_vehicle.py
@@ -138,6 +138,9 @@ class CarlaEgoVehicle(object):
                                                                          spawn_point.location.z,
                                                                          spawn_point.rotation.yaw))
                 if self.player is not None:
+                    spawn_vel = carla.Vector3d()
+                    self.player.set_velocity(spawn_vel)
+                    rospy.sleep(0.5)
                     self.player.set_transform(spawn_point)
                 while self.player is None:
                     self.player = self.world.try_spawn_actor(blueprint, spawn_point)
