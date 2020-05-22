ef4539d2401eb3e7dd8da7440a3b489f0087c077
Diego-ort <diego.ortiz@sttech.de>
2020-05-22 15:20:12 +0200

    Modification of the meta-files of the carla_manual_control and carla_ros_bridge modules

--- carla_ros_bridge/setup.py
+++ carla_ros_bridge/setup.py
@@ -1,13 +1,45 @@
 """
 Setup for carla_ros_bridge
 """
+import os
+ROS_VERSION = int(os.environ['ROS_VERSION'])
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+    if ROS_VERSION == 1:
+    from distutils.core import setup
+    from catkin_pkg.python_setup import generate_distutils_setup
 
-d = generate_distutils_setup(
-    packages=['carla_ros_bridge'],
-    package_dir={'': 'src'}
-)
+    d = generate_distutils_setup(
+        packages=['carla_ros_bridge'],
+        package_dir={'': 'src'}
+    )
 
-setup(**d)
+    setup(**d)
+
+elif ROS_VERSION == 2:
+    from setuptools import setup
+
+    package_name = 'carla_ros_bridge'
+    setup(
+        name=package_name,
+        version='0.0.0',
+        packages=['src/' + package_name],
+        data_files=[
+            ('share/ament_index/resource_index/packages',
+             ['resource/' + package_name]),
+            ('share/' + package_name, ['package.xml']),
+            ('share/' + package_name + '/config',
+             ['config/settings.yaml', 'config/sensors.json'])
+        ],
+        install_requires=['setuptools'],
+        zip_safe=True,
+        maintainer='CARLA Simulator Team',
+        maintainer_email='carla.simulator@gmail.com',
+        description='CARLA ego vehicle for ROS2 bridge',
+        license='MIT',
+        tests_require=['pytest'],
+        entry_points={
+            'console_scripts': [
+                'bridge = carla_ros_bridge.bridge:main'
+            ],
+        },
+    )
