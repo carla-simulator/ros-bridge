aeda9cb8c41abe46278ff16f0b984f9166eb2b11
Arkadiy Telegin <arkadiy.telegin@sttech.de>
2020-05-22 18:44:16 +0200

    Add publisher functionality

--- ros_compatibility/src/ros_compatibility/ros_compatible_node.py
+++ ros_compatibility/src/ros_compatibility/ros_compatible_node.py
@@ -12,9 +12,11 @@ if ROS_VERSION == 1:
 
 
     class CompatibleNode(object):
-        def __init__(self, node_name, queue_size=10):
-            rospy.init_node(node_name, anonymous=True)
+        def __init__(self, node_name, queue_size=10, rospy_init=True):
+            if rospy_init:
+                rospy.init_node(node_name, anonymous=True)
             self.qos_profile = QoSProfile(depth=queue_size)
+            self.callback_group = None
 
         def get_param(self, name, alternative_value=None, alternative_name=None):
             if alternative_value is None:
@@ -24,11 +26,25 @@ if ROS_VERSION == 1:
         def loginfo(self, text):
             rospy.loginfo(text)
 
+        def logwarn(self, text):
+            rospy.logwarn(text)
+
         def logfatal(self, arg):
             rospy.logfatal(arg)
 
+        # assymetry in publisher/subscriber method naming due to rclpy having
+        # create_publisher method.
+        def new_publisher(self, msg_type, topic,
+                            qos_profile=None, callback_group=None):
+            if qos_profile is None:
+                qos_profile = self.qos_profile
+            if callback_group is None:
+                callback_group = self.callback_group
+            return rospy.Publisher(topic, msg_type, queue_size=qos_profile.depth)
+
         def create_subscriber(self, msg_type, topic,
-                              callback, qos_profile=None):
+                              callback, qos_profile=None,
+                              callback_group=None):
             if qos_profile is None:
                 qos_profile = self.qos_profile
             return rospy.Subscriber(topic, msg_type,
@@ -48,11 +64,12 @@ elif ROS_VERSION == 2:
 
 
     class CompatibleNode(Node):
-        def __init__(self, node_name, queue_size=10):
+        def __init__(self, node_name, queue_size=10, rospy_init=True):
             super().__init__(node_name, allow_undeclared_parameters=True,
                              automatically_declare_parameters_from_overrides=True)
             self.logger = self.get_logger()
             self.qos_profile = QoSProfile(depth=queue_size)
+            self.callback_group = None
 
         def get_param(self, name, alternative_value=None, alternative_name=None):
             if alternative_value is None:
@@ -65,15 +82,30 @@ elif ROS_VERSION == 2:
         def loginfo(self, text):
             self.logger.info(text)
 
+        def logwarn(self, text):
+            self.logger.warn(text)
+
         def logfatal(self, arg):
             self.logger.fatal(arg)
 
-        def create_subscriber(self, msg_type, topic,
-                              callback, qos_profile=None):
+        def new_publisher(self, msg_type, topic,
+                            qos_profile=None, callback_group=None):
             if qos_profile is None:
                 qos_profile = self.qos_profile
+            if callback_group is None:
+                callback_group = self.callback_group
+            return self.create_publisher(msg_type, topic,
+                                         qos_profile, callback_group=callback_group)
+
+        def create_subscriber(self, msg_type, topic,
+                              callback, qos_profile=None, callback_group=None):
+            if qos_profile is None:
+                qos_profile = self.qos_profile
+            if callback_group is None:
+                callback_group = self.callback_group
             return self.create_subscription(msg_type, topic,
-                                            callback, qos_profile)
+                                            callback, qos_profile,
+                                            callback_group=callback_group)
 
         def spin(self):
             rclpy.spin(self)
