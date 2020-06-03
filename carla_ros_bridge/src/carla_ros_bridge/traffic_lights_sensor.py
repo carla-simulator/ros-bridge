19410873d06b5c6dd4f1999a9db4243878fe8e42
Dennis Maier <dennis.maier@sttech.de>
2020-06-03 16:37:33 +0200

    Adapt traffic_lights_sensor.py for ROS2 compatibility

--- carla_ros_bridge/src/carla_ros_bridge/traffic_lights_sensor.py
+++ carla_ros_bridge/src/carla_ros_bridge/traffic_lights_sensor.py
@@ -11,8 +11,8 @@ a sensor that reports the state of all traffic lights
 
 from carla_msgs.msg import CarlaTrafficLightStatusList,\
     CarlaTrafficLightInfoList
-from carla_ros_bridge.traffic import TrafficLight
-from carla_ros_bridge.pseudo_actor import PseudoActor
+from src.carla_ros_bridge.traffic import TrafficLight
+from src.carla_ros_bridge.pseudo_actor import PseudoActor
 
 
 class TrafficLightsSensor(PseudoActor):
@@ -31,8 +31,7 @@ class TrafficLightsSensor(PseudoActor):
         :type actor_list: map(carla-actor-id -> python-actor-object)
         """
 
-        super(TrafficLightsSensor, self).__init__(parent=parent,
-                                                  communication=communication,
+        super(TrafficLightsSensor, self).__init__(parent=parent, communication=communication,
                                                   prefix="")
         self.actor_list = actor_list
         self.traffic_light_status = CarlaTrafficLightStatusList()
@@ -68,5 +67,5 @@ class TrafficLightsSensor(PseudoActor):
 
         if traffic_light_status != self.traffic_light_status:
             self.traffic_light_status = traffic_light_status
-            self.publish_message(self.get_topic_prefix() + "traffic_lights",
-                                 traffic_light_status, is_latched=True)
+            self.publish_message(self.get_topic_prefix() + "traffic_lights", traffic_light_status,
+                                 is_latched=True)
