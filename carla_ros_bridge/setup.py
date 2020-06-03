5a4e6fd439de056123c50f1df79da0c4585094fb
sergimoya <sergi.moya@sttech.de>
2020-06-03 09:15:14 +0200

    Add setup.py

--- carla_ros_bridge/setup.py
+++ carla_ros_bridge/setup.py
@@ -2,16 +2,14 @@
 Setup for carla_ros_bridge
 """
 import os
+from glob import glob
 ROS_VERSION = int(os.environ['ROS_VERSION'])
 
 if ROS_VERSION == 1:
     from distutils.core import setup
     from catkin_pkg.python_setup import generate_distutils_setup
 
-    d = generate_distutils_setup(
-        packages=['carla_ros_bridge'],
-        package_dir={'': 'src'}
-    )
+    d = generate_distutils_setup(packages=['carla_ros_bridge'], package_dir={'': 'src'})
 
     setup(**d)
 
@@ -23,12 +21,10 @@ elif ROS_VERSION == 2:
         name=package_name,
         version='0.0.0',
         packages=['src/' + package_name],
-        data_files=[
-            ('share/ament_index/resource_index/packages',
-             ['resource/' + package_name]),
-            ('share/' + package_name, ['package.xml']),
-            (os.path.join('share', package_name), glob('launch/*.launch.py')),
-        ],
+        data_files=[('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+                    ('share/' + package_name, ['package.xml']),
+                    ('share/' + package_name + '/config', ['config/settings.yaml']),
+                    (os.path.join('share', package_name), glob('launch/*.launch.py'))],
         install_requires=['setuptools'],
         zip_safe=True,
         maintainer='CARLA Simulator Team',
@@ -37,8 +33,6 @@ elif ROS_VERSION == 2:
         license='MIT',
         tests_require=['pytest'],
         entry_points={
-            'console_scripts': [
-                'bridge = carla_ros_bridge.bridge:main'
-            ],
+            'console_scripts': ['bridge = src.carla_ros_bridge.bridge:main'],
         },
     )
