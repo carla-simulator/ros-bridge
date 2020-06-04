e94b48b22abd54c88f72e8c35e5e66c3e680ad58
Dennis Maier <dennis.maier@sttech.de>
2020-06-04 17:07:00 +0200

    Change package dir

--- carla_manual_control/setup.py
+++ carla_manual_control/setup.py
@@ -8,10 +8,7 @@ if ROS_VERSION == 1:
     from distutils.core import setup
     from catkin_pkg.python_setup import generate_distutils_setup
 
-    d = generate_distutils_setup(
-        packages=['carla_manual_control'],
-        package_dir={'': 'src'}
-    )
+    d = generate_distutils_setup(packages=['carla_manual_control'], package_dir={'': 'src'})
 
     setup(**d)
 
@@ -22,10 +19,9 @@ elif ROS_VERSION == 2:
     setup(
         name=package_name,
         version='0.0.0',
-        packages=['src/' + package_name],
+        packages=[package_name],
         data_files=[
-            ('share/ament_index/resource_index/packages',
-             ['resource/' + package_name]),
+            ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
             ('share/' + package_name, ['package.xml']),
         ],
         install_requires=['setuptools'],
@@ -36,8 +32,7 @@ elif ROS_VERSION == 2:
         license='MIT',
         tests_require=['pytest'],
         entry_points={
-            'console_scripts': [
-                'manual_control = carla_manual_control.carla_manual_control:main'
-            ],
+            'console_scripts': ['manual_control = carla_manual_control.carla_manual_control:main'],
         },
+        package_dir={'': 'src'},
     )
