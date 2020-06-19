bfba18d65c8d19ec22851d7a27cf6b7120359e64
Dennis Maier <dennis.maier@sttech.de>
2020-06-19 10:16:37 +0200

    Remove unused import

--- carla_ros_bridge/src/carla_ros_bridge/lidar.py
+++ carla_ros_bridge/src/carla_ros_bridge/lidar.py
@@ -28,7 +28,6 @@ if ROS_VERSION == 1:
 elif ROS_VERSION == 2:
     from transforms3d.euler import euler2quat as quaternion_from_euler
     from transforms3d.euler import quat2euler as euler_from_quaternion
-    # from transformations.transformations import euler_from_quaternion, quaternion_from_euler
 else:
     raise NotImplementedError("Make sure you have a valid ROS_VERSION env variable set.")
 
