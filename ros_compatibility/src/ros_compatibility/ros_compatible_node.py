66391325f502583e08d4b283ec00032439b7a870
Kai A. Hiller <hillekia@schaeffler.com>
2020-10-15 10:39:22 +0200

    [ROS2] Fix error handling in carla_ego_vehicle (#381)
    * Fix spelling
    
    * Enable compat spin method to use custom executor
    
    * Improve error handling
    
    * Improve error propagation for loading the sensor-defintion file
    
    * removed python3 specific syntax for ros melodic compatibility
    
    * remove some comments

--- ros_compatibility/src/ros_compatibility/ros_compatible_node.py
+++ ros_compatibility/src/ros_compatibility/ros_compatible_node.py
@@ -20,8 +20,8 @@ if ROS_VERSION == 1:
     def ros_shutdown():
         pass
 
-    def destroy_subscription(subsription):
-        subsription.unregister()
+    def destroy_subscription(subscription):
+        subscription.unregister()
 
     def euler_matrix(roll, pitch, yaw):
         return trans.euler_matrix(roll, pitch, yaw)
@@ -167,8 +167,8 @@ elif ROS_VERSION == 2:
     def ros_shutdown():
         rclpy.shutdown()
 
-    def destroy_subscription(subsription):
-        subsription.destroy()
+    def destroy_subscription(subscription):
+        subscription.destroy()
 
     def euler_matrix(roll, pitch, yaw):
         return euler2mat(roll, pitch, yaw)
@@ -309,7 +309,7 @@ elif ROS_VERSION == 2:
                 return result
 
         def spin(self, executor=None):
-            rclpy.spin(self)
+            rclpy.spin(self, executor)
 
         def get_time(self):
             t = self.get_clock().now()
