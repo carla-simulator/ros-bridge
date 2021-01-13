804a4fc5963e4c56a4f979f1f5acb1c6d5d03c7f
jmagnin <jean-baptiste.magnin@intel.com>
2021-01-13 07:25:43 -0500

    fix same node name for manual_control and carla_ad_agent in ros2 launchfiles

--- carla_manual_control/launch/carla_manual_control.launch.py
+++ carla_manual_control/launch/carla_manual_control.launch.py
@@ -5,24 +5,34 @@ import launch
 import launch_ros.actions
 
 
+def launch_setup(context, *args, **kwargs):
+    # workaround to use launch argument 'role_name' as a part of the string used for the node's name
+    node_name = 'carla_manual_control_' + \
+        launch.substitutions.LaunchConfiguration('role_name').perform(context)
+
+    carla_manual_control = launch_ros.actions.Node(
+        package='carla_manual_control',
+        executable='carla_manual_control',
+        name=node_name,
+        output='screen',
+        emulate_tty=True,
+        parameters=[
+            {
+                'role_name': launch.substitutions.LaunchConfiguration('role_name')
+            }
+        ]
+    )
+
+    return [carla_manual_control]
+
+
 def generate_launch_description():
     ld = launch.LaunchDescription([
         launch.actions.DeclareLaunchArgument(
             name='role_name',
             default_value='ego_vehicle'
         ),
-        launch_ros.actions.Node(
-            package='carla_manual_control',
-            executable='carla_manual_control',
-            name=launch.substitutions.LaunchConfiguration('role_name'),
-            output='screen',
-            emulate_tty=True,
-            parameters=[
-                {
-                    'role_name': launch.substitutions.LaunchConfiguration('role_name')
-                }
-            ]
-        )
+        launch.actions.OpaqueFunction(function=launch_setup)
     ])
     return ld
 
