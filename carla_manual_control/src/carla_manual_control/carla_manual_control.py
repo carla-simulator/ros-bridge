e64bc1f5d7de7e8060692d5a963bbdd13b048a4d
Pasch, Frederik <frederik.pasch@intel.com>
2021-01-18 09:20:47 +0100

    carla_manual_control: fix throttle/brake display

--- carla_manual_control/src/carla_manual_control/carla_manual_control.py
+++ carla_manual_control/src/carla_manual_control/carla_manual_control.py
@@ -554,12 +554,12 @@ class HUD(object):
                     else:
                         rect_border = pygame.Rect((bar_h_offset, v_offset + 8), (bar_width, 6))
                         pygame.draw.rect(display, (255, 255, 255), rect_border, 1)
-                        if item[2] <= 0.0:
-                            f = (item[1] - item[2]) / (item[3] - item[2])
+                        f = (item[1] - item[2]) / (item[3] - item[2])
+                        if item[2] < 0.0:
                             rect = pygame.Rect((bar_h_offset + int(f * (bar_width - 6)), v_offset + 8),
                                                (6, 6))
                         else:
-                            rect = pygame.Rect((bar_h_offset, v_offset + 8), (0, 6))
+                            rect = pygame.Rect((bar_h_offset, v_offset + 8), (int(f * bar_width), 6))
                         pygame.draw.rect(display, (255, 255, 255), rect)
                     item = item[0]
                 if item:  # At this point has to be a str.
