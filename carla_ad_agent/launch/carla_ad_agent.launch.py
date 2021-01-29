e02c8646ea7201e15fdf1a5ccd0e0b6797baaf3c
jmagnin <jean-baptiste.magnin@intel.com>
2021-01-29 08:01:19 -0500

    removed useless workaround to have a composed node name in ros2 launchfile

--- carla_ad_agent/launch/carla_ad_agent.launch.py
+++ carla_ad_agent/launch/carla_ad_agent.launch.py
@@ -4,33 +4,6 @@ import sys
 import launch
 import launch_ros.actions
 
-
-def launch_setup(context, *args, **kwargs):
-    # workaround to use launch argument 'role_name' as a part of the string used for the node's name
-    node_name = 'carla_ad_agent_' + \
-        launch.substitutions.LaunchConfiguration('role_name').perform(context)
-
-    carla_ad_demo = launch_ros.actions.Node(
-        package='carla_ad_agent',
-        executable='carla_ad_agent',
-        name=node_name,
-        output='screen',
-        parameters=[
-            {
-                'target_speed': launch.substitutions.LaunchConfiguration('target_speed')
-            },
-            {
-                'role_name': launch.substitutions.LaunchConfiguration('role_name')
-            },
-            {
-                'avoid_risk': launch.substitutions.LaunchConfiguration('avoid_risk')
-            }
-        ]
-    )
-
-    return [carla_ad_demo]
-
-
 def generate_launch_description():
     ld = launch.LaunchDescription([
         launch.actions.DeclareLaunchArgument(
@@ -49,7 +22,23 @@ def generate_launch_description():
             name='avoid_risk',
             default_value='True'
         ),
-        launch.actions.OpaqueFunction(function=launch_setup)
+        launch_ros.actions.Node(
+            package='carla_ad_agent',
+            executable='carla_ad_agent',
+            name=['carla_ad_agent_', launch.substitutions.LaunchConfiguration('role_name')],
+            output='screen',
+            parameters=[
+                {
+                    'target_speed': launch.substitutions.LaunchConfiguration('target_speed')
+                },
+                {
+                    'role_name': launch.substitutions.LaunchConfiguration('role_name')
+                },
+                {
+                    'avoid_risk': launch.substitutions.LaunchConfiguration('avoid_risk')
+                }
+            ]
+        )
     ])
     return ld
 
