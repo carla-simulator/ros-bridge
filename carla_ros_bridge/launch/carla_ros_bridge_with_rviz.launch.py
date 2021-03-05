3daca98ace807a56f1d77bb39f15733a12688da6
Joel Moriana <joel.moriana@gmail.com>
2021-03-05 13:32:59 +0100

    fixed package name rviz

--- carla_ros_bridge/launch/carla_ros_bridge_with_rviz.launch.py
+++ carla_ros_bridge/launch/carla_ros_bridge_with_rviz.launch.py
@@ -8,9 +8,9 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
     ld = launch.LaunchDescription([
         launch_ros.actions.Node(
-            package='rviz',
-            executable='rviz',
-            name='rviz'
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2'
         ),
         launch.actions.IncludeLaunchDescription(
             launch.launch_description_sources.PythonLaunchDescriptionSource(
