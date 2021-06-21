35d922de0175142228aae292d49a022ea8b24891
Joel Moriana <joel.moriana@gmail.com>
2021-06-21 13:22:26 +0200

    Add description field to launch arguments

--- carla_ros_bridge/launch/carla_ros_bridge.launch.py
+++ carla_ros_bridge/launch/carla_ros_bridge.launch.py
@@ -6,44 +6,54 @@ def generate_launch_description():
     ld = launch.LaunchDescription([
         launch.actions.DeclareLaunchArgument(
             name='host',
-            default_value='localhost'
+            default_value='localhost',
+            description='IP of the CARLA server'
         ),
         launch.actions.DeclareLaunchArgument(
             name='port',
-            default_value='2000'
+            default_value='2000',
+            description='TCP port of the CARLA server'
         ),
         launch.actions.DeclareLaunchArgument(
             name='timeout',
-            default_value='2'
+            default_value='2',
+            description='Time to wait for a successful connection to the CARLA server'
         ),
         launch.actions.DeclareLaunchArgument(
             name='passive',
-            default_value='False'
+            default_value='False',
+            description='When enabled, the ROS bridge will take a backseat and another client must tick the world (only in synchronous mode)'
         ),
         launch.actions.DeclareLaunchArgument(
             name='synchronous_mode',
-            default_value='True'
+            default_value='True',
+            description='Enable/disable synchronous mode. If enabled, the ROS bridge waits until the expected data is received for all sensors'
         ),
         launch.actions.DeclareLaunchArgument(
             name='synchronous_mode_wait_for_vehicle_control_command',
-            default_value='False'
+            default_value='False',
+            description='When enabled, pauses the tick until a vehicle control is completed (only in synchronous mode)'
         ),
         launch.actions.DeclareLaunchArgument(
             name='fixed_delta_seconds',
-            default_value='0.05'
+            default_value='0.05',
+            description='Simulation time (delta seconds) between simulation steps'
         ),
         launch.actions.DeclareLaunchArgument(
             name='town',
-            default_value='Town01'
+            default_value='Town01',
+            description='Either use an available CARLA town (eg. "Town01") or an OpenDRIVE file (ending in .xodr)'
         ),
         launch.actions.DeclareLaunchArgument(
             name='register_all_sensors',
-            default_value='True'
+            default_value='True',
+            description='Enable/disable the registration of all sensors. If disabled, only sensors spawned by the bridge are registered'
         ),
         launch.actions.DeclareLaunchArgument(
             name='ego_vehicle_role_name',
             default_value=["hero", "ego_vehicle", "hero0", "hero1", "hero2",
-                           "hero3", "hero4", "hero5", "hero6", "hero7", "hero8", "hero9"]
+                           "hero3", "hero4", "hero5", "hero6", "hero7", "hero8", "hero9"],
+            description='Role names to identify ego vehicles. '
         ),
         launch_ros.actions.Node(
             package='carla_ros_bridge',
