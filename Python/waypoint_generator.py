a942422b07db3566794f2e8e8d4a402a16e03618
HanJongSoo <jongsoo@g.skku.edu>
2023-12-02 21:22:02 +0900

    config file

--- Python/waypoint_generator.py
+++ Python/waypoint_generator.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 
 import carla
+import time
 
 from agents.navigation.global_route_planner import GlobalRoutePlanner
 # from agents.navigation.global_route_planner_dao import GlobalRoutePlannerDAO
-
-
-
-
 
 def main():
 
@@ -17,11 +14,13 @@ def main():
     world = client.get_world()
 
     amap = world.get_map()
-    sampling_resolution = 2
+    sampling_resolution = 3
     grp = GlobalRoutePlanner(amap, sampling_resolution)
     # grp = GlobalRoutePlanner(dao)
     # dao.setup()
     spawn_points = amap.get_spawn_points()
+    print(len(spawn_points))
+    print('\n')
     a = carla.Location(spawn_points[0].location)
     b = carla.Location(spawn_points[1].location)
 
@@ -31,7 +30,8 @@ def main():
         world.debug.draw_string(w[0].transform.location, 'O', draw_shadow=False,
         color=carla.Color(r=0, g=0, b=255), life_time=20.0,
         persistent_lines=True)
-        print(w)
+        print(w[0].transform.location)
+        time.sleep(0.1)
 
     print('\nBye!')
 
