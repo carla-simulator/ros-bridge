2c3e2a6c959eb050b4b485cf062bf5e3a555e809
ttgamage <1236540+ttgamage@users.noreply.github.com>
2024-01-18 16:00:00 -0600

    Force --sync mode for Scenario Runner

--- carla_ros_scenario_runner/src/carla_ros_scenario_runner/scenario_runner_runner.py
+++ carla_ros_scenario_runner/src/carla_ros_scenario_runner/scenario_runner_runner.py
@@ -39,8 +39,9 @@ class ScenarioRunnerRunner(ApplicationRunner):
             python_path = "/usr/bin/python3"
         cmdline = [python_path, "{}/scenario_runner.py".format(self._path),
                    "--openscenario", "{}".format(scenario_file),
-                   "--timeout", "1000000",
+                   "--timeout", "1000",
                    "--host", self._host,
+                   "--sync",
                    "--port", str(self._port)]
         if self._wait_for_ego:
             cmdline.append("--waitForEgo")
